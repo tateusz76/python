f0991d5e082969181ee91c6507da62d35279a473
tateusz76 <59434750+tateusz76@users.noreply.github.com>
2020-02-26 18:25:32 +0100

    Add files via upload

---
+++ start.py
@@ -0,0 +1,41 @@
+print("Hello world!")
+
+# komentarz
+
+a = 5
+#print(type(a))
+
+imie = "ala"
+imie = imie.capitalize()
+
+#print(imie + imie)
+
+#print(5 + imie)
+
+# formatowanie wyjścia
+wiek = 5
+print(f"{imie} ma {wiek} lat")
+
+print("{} ma {} lat".format(imie, 5))
+
+# listy
+
+lista = []
+lista = [1,4, "Ala", 4.5, imie, [1,2]]
+print(lista)
+lista[5][1]
+
+lista.append(3)
+print(lista)
+
+lista[0] = 2
+print(lista)
+
+lista2 = lista + lista
+print(lista2)
+
+lista_nowa = list()
+
+# słownik
+
+
