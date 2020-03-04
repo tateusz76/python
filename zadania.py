476688f4f31bb7344847d6bc1a0555942a5fa336
Tateusz <traczyk.mateusz76@gmail.com>
2020-03-04 18:23:55 +0100

    commit

--- zadania.py
+++ zadania.py
@@ -1,5 +1,7 @@
 #pierwiastek
-# import math  
+
+import math 
+import sys 
 
 # x = int(input())
 # if x < 0:
@@ -12,8 +14,39 @@
 
 
 #czylitera/cyfra
-y = input()
-if y.isdigit():
-    print("Jest to cyfra/liczba")
-else:
-    print("Jest to litera/ciąg liter")
+
+# y = input()
+# if y.isdigit():
+#     print("Jest to cyfra/liczba")
+# else:
+#     print("Jest to litera/ciąg liter")
+
+
+#spacje
+
+# text = input("Podaj tekst: ")
+# spaceCounter = 0
+
+# for length in text:
+#     if length == " ":
+#         spaceCounter += 1
+
+# print("Ilość spacji wynosi: ", end = '')
+# print(spaceCounter)
+
+
+#podzielne 5
+
+# limit = int(input("Podaj limit pętli: "))
+# x = range(limit)
+# for i in x:
+#     if i%5==0:
+#         print(i)
+
+#kwadraty
+
+# limit = int(input("Podaj ile liczb chcesz spotęgować: "))
+# x = range(limit)
+# for i in x:
+#     l = int(input("Podaj liczbe, wyświetli się jej kwadrat "))
+#     print(l*l)
