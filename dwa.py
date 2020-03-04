72adc84f23233b059490e0711c487941c9297f96
Tateusz <traczyk.mateusz76@gmail.com>
2020-03-04 17:17:39 +0100

    commit

--- dwa.py
+++ dwa.py
@@ -1,6 +1,42 @@
-wejscie = input()
-print(wejscie)
-print(type(wejscie))
+#wejscie = input()
+#print(wejscie)
+#print(type(wejscie))
 
-import sys
-sys.
+#import sys
+#wejscie2 = sys.stdin.readline()
+#print(wejscie2)
+#print(type(wejscie2))
+
+
+
+# liczba = 10
+# if liczba < 10:
+#     print('cośtam1')
+# elif liczba > 0:
+#     print('cośtam2')
+# else:
+#     print('klapa')
+
+#and or not
+# if liczba > 0 and liczba < 11:
+#      print('OK') 
+
+#krócej
+# if  0 < liczba < 11:
+#      print('OK')     
+
+
+#foreach
+imie = 'Ariadna'
+# for litera in imie:
+#     print(litera, end = '')
+
+#range
+range(5) #start, stop, step
+print(range(5))
+print(list(range(2,10,2))) #stop = stop - step
+print(list(range(10,2,-2)))
+
+#[od:do]
+print(imie[2:4])
+print(imie[::2])
